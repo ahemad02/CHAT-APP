078be4a33892f104518b355cf039918364db9187
Ahemad <ahemadbakali26@gmail.com>
2025-06-21 18:02:56 +0530

    "second commit"

--- backend/server.js
+++ backend/server.js
@@ -61,8 +61,12 @@ app.use("/api/messages", messageRouter);
 
 await connectDB();
 
-const PORT = process.env.PORT || 3000;
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+    server.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    })
+}
+
+export default server;
